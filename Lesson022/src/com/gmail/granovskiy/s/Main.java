a9d4a2a9b5e851c301ee3c64ea64cfbe85e3cc04
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 16:25:45 +0200

    Initial commit

---
+++ Lesson022/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,34 @@
+/*
+ * CONDITIONALS AND CONTROL FLOW 
+ * Switch Statement
+ * Set the code block under the default case to:
+ * System.out.println("Messi is in position...");
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		char penaltyKick = 'X';
+
+		switch (penaltyKick) {
+
+		case 'L':
+			System.out.println("Messi shoots to the left and scores!");
+			break;
+		case 'R':
+			System.out.println("Messi shoots to the right and misses the goal!");
+			break;
+		case 'C':
+			System.out.println("Messi shoots down the center, but the keeper blocks it!");
+			break;
+		default:
+			System.out.println("Messi is in position...");
+
+		}
+
+	}
+
+}
