d2a18641e116d5d3c20a46653a4b8f6080cad436
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 13:50:05 +0200

    Initial commit

---
+++ Lesson011/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Equality Operators
+ * Use any equality operator to directly compare two Boolean values. 
+ * Do not declare any variables.
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(true != false);
+
+	}
+
+}
