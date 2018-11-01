e32a3b68f6767d4c83c87f65617c96970162acdb
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 13:24:39 +0200

    Initial commit

---
+++ Lesson009/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Math: %
+ * Use the modulo operator to set the variable myRemainder equal to 2. 
+ * You can use any two numbers that return a remainder value of 2.
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int myRemainder = 14 % 3;
+		System.out.println(myRemainder);
+
+	}
+
+}
