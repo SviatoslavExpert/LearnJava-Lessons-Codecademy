d42bb0ba0d3f983ba1f73946ca4dd97f27180c24
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 14:42:27 +0200

    Initial commit

---
+++ Lesson015/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Boolean Operators: ||
+ * Use the || operator and any two Boolean expressions 
+ * of your choice between the parentheses of System.out.println(); 
+ * in order to print out a value of true or false to the console.
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(true || false);
+
+	}
+
+}
