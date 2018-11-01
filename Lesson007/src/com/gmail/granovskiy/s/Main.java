328f740d084d2190c3e8a455ac778ce3a0f71842
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 12:36:59 +0200

    Initial commit

---
+++ Lesson007/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Learning commenting in Java
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// System.out.println("Noise!");
+
+		/*
+		 * In this section I learned commenting in Java
+		 */
+
+	}
+
+}
