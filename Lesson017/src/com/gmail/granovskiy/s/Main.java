e837cfe6af62fa1a7e7778b79169d8242545b348
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 15:46:22 +0200

    Initial commit

---
+++ Lesson017/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,21 @@
+/*
+ * CONDITIONALS AND CONTROL FLOW
+ * Boolean Operators: Precedence
+ * Line 4 has a code statement that is incomplete. 
+ * Use each Boolean operator no more than once to replace 
+ * the empty comments. 
+ * The code statement should print out false.
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		boolean riddle = !( 1 < 8 || (5 > 2 && 3 < 5));
+		System.out.println(riddle);
+
+	}
+
+}
