59920890c9399fb47fc3b45780193bd0de9de3bc
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 10:04:45 +0200

    Initial commit

---
+++ Lesson006/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,12 @@
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		boolean isFormatted = false;
+	    System.out.println(isFormatted);
+
+	}
+
+}
