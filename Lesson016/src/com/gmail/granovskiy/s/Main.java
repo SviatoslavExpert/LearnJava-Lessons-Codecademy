338447853ff17ac6a63567083d043d5a1bfccc97
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 14:48:16 +0200

    Initial commit

---
+++ Lesson016/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,19 @@
+/*
+ * CONDITIONALS AND CONTROL FLOW
+ * Boolean Operators: !
+ * 1. Use the ! operator to return a value of true for the code on line 14.
+ * 2. Use the ! operator to return a value of false for the code on line 15.
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(!false);
+		System.out.println(!(5 >= 1));
+
+	}
+
+}
