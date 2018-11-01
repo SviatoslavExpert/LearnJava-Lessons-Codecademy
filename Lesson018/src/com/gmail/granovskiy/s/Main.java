0391f21280de79ee69667175c4fd687cc58fee0f
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 15:55:56 +0200

    Initial commit

---
+++ Lesson018/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,20 @@
+/*
+ * CONDITIONALS AND CONTROL FLOW
+ * If Statement
+ * The if statement in the code editor is missing its Boolean expression. 
+ * Provide the if statement with a Boolean expression that evaluates to true.
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		if (true || false) {
+			System.out.println("Access granted.");
+		}
+
+	}
+
+}
