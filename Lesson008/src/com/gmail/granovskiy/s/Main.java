c9109b603c9e160754efa1acf2597101860755b1
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 12:45:41 +0200

    Initial commit

---
+++ Lesson008/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Math: +, -, *, and /
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int myNumber = 10 * 15;
+		int sum = 34 + 113;
+		int difference = 91 - 205;
+		int product = 2 * 8;
+		int quotient = 45 / 3;
+
+		System.out.println(myNumber + ", " + sum + ", " + difference + ", " + product + ", " + quotient);
+
+	}
+
+}
