d3e730f573afb344d99097fc644ea39ddf666232
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 13:42:53 +0200

    Initial commit

---
+++ Lesson010/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Relational Operators
+ * Use one of the relational operators to compare any two integers.
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(10 < 12);
+
+	}
+
+}
