243037464870951de241602a19d815adb48b1df5
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-01 16:03:14 +0200

    Initial commit

---
+++ Lesson019/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,24 @@
+/*
+ * CONDITIONALS AND CONTROL FLOW 
+ * If-Else Statement
+ * The if/else statement in the code editor currently prints out 
+ * the code in the if block. Modify the if statement's Boolean 
+ * expression so that the code in the else block gets executed 
+ * and prints to the console.
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		if (7 < 7) {
+			System.out.println("Try again...");
+		} else {
+			System.out.println("Success!");
+		}
+
+	}
+
+}
