a532ad81ef144b8aeee9cb9d6705e61be600d568
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-03 10:42:53 +0200

    Initial commit

---
+++ Lesson030/src/com/gmai/granovskiy/s/Dog.java
@@ -0,0 +1,27 @@
+/*
+ * OBJECT-ORIENTED JAVA 
+ * Methods: I
+ * In between the constructor and the main method, 
+ * add a method called bark to the Dog class by typing:
+ * public void bark() { }
+ */
+
+package com.gmai.granovskiy.s;
+
+public class Dog {
+
+	int age;
+
+	public Dog(int dogsAge) {
+		age = dogsAge;
+	}
+
+	public void bark() {
+		System.out.println("Woof!");
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
