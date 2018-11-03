2e6ad8ecc18a40f771f1886496ab5aef8511c1e4
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-03 10:03:23 +0200

    Initial commit

---
+++ Lesson026/src/com/gmail/granovskiy/s/Dog.java
@@ -0,0 +1,23 @@
+/*
+ *  OBJECT-ORIENTED JAVA 
+ *  Classes: Instance Variables
+ *  Let's model the Dog class after a real-world dog. 
+ *  Create an int instance variable called age.
+ *  
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Dog {
+
+	int age;
+
+	public Dog() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
