67cec497e504b8cc1e81a53952b1fd6be10eca72
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-03 10:24:08 +0200

    Initial commit

---
+++ Lesson028/src/com/gmail/granovskiy/s/Dog.java
@@ -0,0 +1,25 @@
+/*
+ * OBJECT-ORIENTED JAVA
+ * The main Method
+ * Your Dog class already has a main method. 
+ * It should look like the following:
+ * public static void main(String[] args) { }
+ * Do not include anything inside of the main method yet. 
+ * Click Run to continue.
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Dog {
+
+	int age;
+
+	public Dog(int dogsAge) {
+		age = dogsAge;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
