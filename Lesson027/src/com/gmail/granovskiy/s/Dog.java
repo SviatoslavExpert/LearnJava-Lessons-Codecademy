8c442900d8b7f0c98e3e40d29f018c4f620747ee
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-03 10:16:55 +0200

    Initial commit

---
+++ Lesson027/src/com/gmail/granovskiy/s/Dog.java
@@ -0,0 +1,25 @@
+/*
+ * OBJECT-ORIENTED JAVA
+ * Classes: Constructor Parameters
+ * 1. Add an int parameter 
+ * called dogsAge in between the parentheses 
+ * of the Dog() constructor.
+ * 2. Inside of the constructor block, 
+ * set the instance variable age equal to the dogsAge parameter
+ */
+
+package com.gmail.granovskiy.s;
+
+public class Dog {
+
+	int age;
+
+	public Dog(int dogsAge) {
+		age = dogsAge;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
