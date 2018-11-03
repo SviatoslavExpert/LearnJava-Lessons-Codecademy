6e6cb65922f9e289cfb4998093288d41b0676917
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-03 09:45:47 +0200

    Initial commit

---
+++ Lesson025/src/com/gmail/granovskiy/s/Dog.java
@@ -0,0 +1,15 @@
+/*
+ *  In the code editor, create a Dog class. Use the example above 
+ *  to help you. For now, you can ignore the line(s) of code 
+ *  at the bottom. We will return to it later in this lesson.
+ */
+
+package com.gmail.granovskiy.s;
+
+class Dog {
+
+	public static void main(String[] args) {
+
+	}
+
+}
