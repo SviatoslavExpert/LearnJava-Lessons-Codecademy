a1c7fd7864ca844b085debdcae793d9cc9725f6d
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-04 09:27:20 +0200

    Initial commit

---
+++ Lesson037/src/com/gmail/granovskiy/s/For.java
@@ -0,0 +1,23 @@
+/*
+ * DATA STRUCTURES 
+ * For Loop
+ * The for loop in the code editor is currently missing the three statements 
+ * required to execute the code in the block. Finish the for loop by typing:
+ * for (int waterLevel = 0; waterLevel < 7; waterLevel++) { }
+ */
+
+package com.gmail.granovskiy.s;
+
+public class For {
+
+	public static void main(String[] args) {
+
+		for (int waterLevel = 0; waterLevel < 7; waterLevel++) {
+
+			System.out.println("The pool's water level is at " + waterLevel + " feet.");
+
+		}
+
+	}
+
+}
