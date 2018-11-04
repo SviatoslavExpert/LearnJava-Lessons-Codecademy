f5e0f79cad8931d79644aeb312632ff62097d586
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-04 09:36:18 +0200

    Initial commit

---
+++ Lesson038/src/com/gmail/granovskiy/s/Temperatures.java
@@ -0,0 +1,18 @@
+/*
+ * DATA STRUCTURES 
+ * ArrayList
+ * Inside of the main method, create an ArrayList object 
+ * called weeklyTemperatures that will store Integer data types.
+ */
+
+package com.gmail.granovskiy.s;
+
+import java.util.ArrayList;
+
+public class Temperatures {
+	
+	public static void main(String[] args) {
+      ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
+
+	}
+}
