d07b36ee3de836726fb38a3a0c3598e45a9f5449
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-04 09:39:38 +0200

    Initial commit

--- Lesson038/src/com/gmail/granovskiy/s/Temperatures.java
+++ Lesson038/src/com/gmail/granovskiy/s/Temperatures.java
@@ -10,9 +10,9 @@ package com.gmail.granovskiy.s;
 import java.util.ArrayList;
 
 public class Temperatures {
-	
+
 	public static void main(String[] args) {
-      ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
+		ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
 
 	}
 }
