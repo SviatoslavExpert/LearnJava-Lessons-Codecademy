3ce7ef63838505a20bb146f58cd87f21607142a2
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-04 10:57:25 +0200

    Initial commit

---
+++ Lesson044/src/com/gmail/granovskiy/s/Restaurant.java
@@ -0,0 +1,19 @@
+/*
+ * DATA STRUCTURES  
+ * HashMap
+ * Create a HashMap object called restaurantMenu. 
+ * The HashMap should store String keys and Integer values. 
+ */
+
+package com.gmail.granovskiy.s;
+
+import java.util.HashMap;
+
+public class Restaurant {
+
+	public static void main(String[] args) {
+		
+		HashMap<String, Integer> restaurantMenu = new HashMap<String, Integer>();
+
+	}
+}
