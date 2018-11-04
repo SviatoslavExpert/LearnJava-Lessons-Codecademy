6190da87a2cb928905abf0f53dcb5a239998762c
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-04 09:48:32 +0200

    Initial commit

---
+++ Lesson039/src/com/gmail/granovskiy/s/Temperatures.java
@@ -0,0 +1,26 @@
+/*
+ * DATA STRUCTURES  
+ * ArrayList: Manipulation
+ *  1. Add some temperatures to weeklyTemperatures. 
+ *    First, add a temperature value of 78.
+ *  2. Next, add a temperature of 67.
+ *  3. Add another temperature of 89.
+ *  4. Finally, add a temperature of 94.
+ */
+
+package com.gmail.granovskiy.s;
+
+import java.util.ArrayList;
+
+public class Temperatures {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
+		weeklyTemperatures.add(78);
+		weeklyTemperatures.add(67);
+		weeklyTemperatures.add(89);
+		weeklyTemperatures.add(94);
+
+	}
+}
